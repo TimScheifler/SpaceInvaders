571f8e7009bb7498965a8523908f197a17a8f85c
TimScheifler <tim.scheifler@web.de>
2020-03-14 18:56:02 +0000

    Added a language preference to SharedPreferencesHandler.

--- app/src/main/java/com/madproject/spaceinvaders/SharedPreferencesHandler.java
+++ app/src/main/java/com/madproject/spaceinvaders/SharedPreferencesHandler.java
@@ -7,6 +7,10 @@ import static android.content.Context.MODE_PRIVATE;
 
 public class SharedPreferencesHandler {
 
+    private static final String MY_PREFS_LANGUAGE = "SPACE_INVADERS_LANGUAGE";
+    private static final String GAME_LANGUAGE = "language";
+    private static final String DEFAULT_LANGUAGE = "de";
+
     private static final String MY_PREFS_NAME = "SPACE_INVADERS_GAME_PLAYER_NAME";
     private static final String PLAYER_NAME = "name";
     private static final String DEFAULT_NAME = "Player";
@@ -31,6 +35,17 @@ public class SharedPreferencesHandler {
     public void applySharedPrefsSoundOn(boolean soundOn){
         editor = context.getSharedPreferences(MY_PREFS_SOUND_ON, MODE_PRIVATE).edit();
         editor.putBoolean(SOUND_ON, soundOn);
+        editor.apply();
+    }
+
+    public String getSharedPrefsLanguage(){
+        prefs = context.getSharedPreferences(MY_PREFS_LANGUAGE, MODE_PRIVATE);
+        return prefs.getString(GAME_LANGUAGE, DEFAULT_LANGUAGE);
+    }
+
+    public void applySharedPrefsLanguage(String language){
+        editor = context.getSharedPreferences(MY_PREFS_LANGUAGE, MODE_PRIVATE).edit();
+        editor.putString(GAME_LANGUAGE, language);
         editor.apply();
     }
 
